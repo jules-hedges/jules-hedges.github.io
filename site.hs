7364056e7195533ca8c2edfc4d0ce37bd5fb944d
Jules Hedges <juleshedges.invariant@gmail.com>
2025-10-19 18:07:30 +0100

    Refactor and comment haskell source

--- site.hs
+++ site.hs
@@ -1,88 +1,98 @@
 {-# LANGUAGE OverloadedStrings #-}
 
-import Data.Monoid (mappend)
 import Text.Pandoc
 import Hakyll
 
+-- Custom Hakyll config
 config :: Configuration
 config = defaultConfiguration {
-    destinationDirectory = "docs"
+  -- Github Pages has a hardcoded requirement that the site is in docs/
+  destinationDirectory = "docs"
 }
 
-myPandocCompiler :: Compiler (Item String)
-myPandocCompiler = pandocCompilerWith ropt wopt
-  where ropt = defaultHakyllReaderOptions
-        wopt = defaultHakyllWriterOptions {
-          writerExtensions = writerExtensions defaultHakyllWriterOptions <> pandocExtensions,
-          writerHTMLMathMethod = MathML
-        }
+-- Custom Pandoc settings
+getPandocCompiler :: Compiler (Item String)
+getPandocCompiler = pandocCompilerWith readerOptions writerOptions where
+  readerOptions = defaultHakyllReaderOptions
+  writerOptions = defaultHakyllWriterOptions {
+    -- Enables Pandoc flavoured markdown
+    writerExtensions = pandocExtensions <> writerExtensions defaultHakyllWriterOptions,
+    -- Use MathML for mathmode rendering (not KaTeX or MathJax!)
+    writerHTMLMathMethod = MathML
+  }
 
+-- Template context for blog posts
+postContext :: Context String
+postContext = dateField "date" "%B %e, %Y" <> defaultContext
+
+-- Hakyll entry point
 main :: IO ()
 main = hakyllWith config $ do
 
-    match "CNAME" $ do
-        route   idRoute
-        compile copyFileCompiler
+  -- Assorted root directory files that need to be copied verbatim
+  match (fromList ["CNAME"]) $ do
+    route idRoute
+    compile copyFileCompiler
 
-    match "assets/**" $ do
-        route   idRoute
-        compile copyFileCompiler
+  -- Assets folder needs to be copied unchanged
+  match "assets/**" $ do
+    route idRoute
+    compile copyFileCompiler
 
-    match "css/*" $ do
-        route   idRoute
-        compile compressCssCompiler
+  -- The output of Forester is hardcoded to output/
+  -- so we need to just copy it into the right place
+  -- Because of this, we have to build Hakyll after building Forester in the makefile
+  match "output/**" $ do
+    route $ gsubRoute "output/" $ const ""
+    compile copyFileCompiler
 
-    match (fromList ["papers.md", "links.md", "404.md"]) $ do
-        route   $ setExtension "html"
-        compile $ myPandocCompiler
-            >>= loadAndApplyTemplate "templates/default.html" defaultContext
-            >>= relativizeUrls
+  -- CSS files
+  match "css/*" $ do
+    route idRoute
+    compile compressCssCompiler
 
-    -- The output of Forester is hardcoded to output/
-    -- so we need to just copy it into the right place
-    -- Because of this, we have to build Hakyll after building Forester in the makefile
-    match "output/**" $ do
-        route   (gsubRoute "output/" (const ""))
-        compile copyFileCompiler
+  -- Hakyll templates
+  match "templates/*" $ do
+    compile templateBodyCompiler
 
-    match "posts/*" $ do
-        route $ setExtension "html"
-        compile $ myPandocCompiler
-            >>= loadAndApplyTemplate "templates/post.html"    postCtx
-            >>= loadAndApplyTemplate "templates/default.html" postCtx
-            >>= relativizeUrls
+  -- Root directory markdown files with no special handling
+  match (fromList ["papers.md", "links.md", "404.md"]) $ do
+    route $ setExtension "html"
+    compile $ do
+      pandoc          <- getPandocCompiler
+      defaultTemplate <- loadAndApplyTemplate "templates/default.html" postContext pandoc
+      relativizeUrls defaultTemplate
 
-    create ["blog.html"] $ do
-        route idRoute
-        compile $ do
-            posts <- recentFirst =<< loadAll "posts/*"
-            let archiveCtx =
-                    listField "posts" postCtx (return posts) `mappend`
-                    constField "title" "Blog"                `mappend`
-                    defaultContext
+  -- All blog posts
+  match "posts/*" $ do
+    route $ setExtension "html"
+    compile $ do
+      pandoc          <- getPandocCompiler
+      postTemplate    <- loadAndApplyTemplate "templates/post.html"    postContext pandoc
+      defaultTemplate <- loadAndApplyTemplate "templates/default.html" postContext postTemplate
+      relativizeUrls defaultTemplate
 
-            makeItem ""
-                >>= loadAndApplyTemplate "templates/blog.html" archiveCtx
-                >>= loadAndApplyTemplate "templates/default.html" archiveCtx
-                >>= relativizeUrls
+  -- Blog posts page
+  create ["blog.html"] $ do
+    route idRoute
+    compile $ do
+      allPosts        <- loadAll "posts/*"
+      sortedPosts     <- recentFirst allPosts
+      let context = listField "posts" postContext (pure sortedPosts) <> constField "title" "Blog" <> defaultContext
+      newPage         <- makeItem ""
+      blogTemplate    <- loadAndApplyTemplate "templates/blog.html"    context newPage
+      defaultTemplate <- loadAndApplyTemplate "templates/default.html" context blogTemplate
+      relativizeUrls defaultTemplate
 
-    match "index.md" $ do
-        route   $ setExtension "html"
-        compile $ do
-            posts <- fmap (take 10) (recentFirst =<< loadAll "posts/*")
-            let indexCtx =
-                    listField "posts" postCtx (return posts) `mappend`
-                    defaultContext
-            myPandocCompiler
-                >>= applyAsTemplate indexCtx
-                >>= loadAndApplyTemplate "templates/default.html" indexCtx
-                >>= relativizeUrls
-
-    match "templates/*" $ compile templateBodyCompiler
-
-
---------------------------------------------------------------------------------
-postCtx :: Context String
-postCtx =
-    dateField "date" "%B %e, %Y" `mappend`
-    defaultContext
+  -- Index page
+  match "index.md" $ do
+    route $ setExtension "html"
+    compile $ do
+      allPosts        <- loadAll "posts/*"
+      sortedPosts     <- recentFirst allPosts
+      let recentPosts = take 10 sortedPosts
+      let context = listField "posts" postContext (pure recentPosts) <> defaultContext
+      pandoc          <- getPandocCompiler
+      indexTemplate   <- applyAsTemplate context pandoc
+      defaultTemplate <- loadAndApplyTemplate "templates/default.html" context indexTemplate
+      relativizeUrls defaultTemplate
